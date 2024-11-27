d9c1c51043025a94d1c1002f6543ff7a01560c3d
AkashHirumal <akashhirumal@gmail.com>
2024-11-27 15:43:51 +0530

    4th commit

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
+let firstDetails = document.getElementById("firstDetails");
 
-fetch("./soc.json")
+
+fetch("./details.json")
   .then(res => res.json())
   .then(data2 => {
+
+
+const contentPerformance = data2[3]; 
+
+console.log(contentPerformance);
+
+contentPerformance.topPosts.forEach(post => {
+  firstDetails.innerHTML += `
+  <div class="col" >
+    <div class="card topcrd">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="${post.photo}" class="img-fluid rounded-start topimg" alt="Post">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title topptitle">${post.title}</h5>
+            <p class="card-text">
+              Engagement: ${post.engagement}
+              <i class="bi bi-bar-chart-line-fill"></i>
+              <br>
+              Reach: ${post.reach}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+  `;
+});
 
     console.log(data2);
 
@@ -20,22 +52,22 @@ new Chart(ctx, {
       label: 'Display',
       data: ufollowers,
       backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-        'rgba(255, 205, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(201, 203, 207, 0.2)'
+        '#0088ff',
+        '#0088ff',
+        '#0088ff',
+        '#0088ff',
+        '#0088ff',
+        '#0088ff',
+        '#0088ff'
       ],
       borderColor: [
-        'rgb(255, 99, 132)',
-        'rgb(255, 159, 64)',
-        'rgb(255, 205, 86)',
-        'rgb(75, 192, 192)',
-        'rgb(54, 162, 235)',
-        'rgb(153, 102, 255)',
-        'rgb(201, 203, 207)'
+        '#000000',
+        '#000000',
+        '#000000',
+        '#000000',
+        '#000000',
+        '#000000',
+        '#000000'
       ],
       borderWidth: 1
     }]
